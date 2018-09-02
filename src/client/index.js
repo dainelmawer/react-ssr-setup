e9c7c36514ac59b143b75d4947656825954d9095
Daine Mawer <dainemawer@Daines-MacBook-Pro.local>
2018-09-02 12:09:17 +0200

    Client side routing

---
+++ src/client/index.js
@@ -0,0 +1,13 @@
+/* global document */
+import React from 'react';
+import { hydrate } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from '../shared/App';
+
+hydrate(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app'),
+);
